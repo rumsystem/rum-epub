0aacff69b7d4c244f6d612708108d58b002fc209
noe132 <nnnnoe132@gmail.com>
2022-05-16 16:57:51 +0800

    fix: preflight style

--- src/index.tsx
+++ src/index.tsx
@@ -13,22 +13,5 @@ if (process.env.IS_ELECTRON) {
   ipcRenderer.setMaxListeners(20);
 }
 
-if (process.env.NODE_ENV === 'production') {
-  import('./styles/tailwind-base.sass?inline').then((v) => {
-    const injectStyleTo = (comment: string, css: string) => {
-      const style = document.createElement('style');
-      style.innerHTML = css;
-      const commentNode = Array.from(document.head.childNodes)
-        .filter((v) => v.nodeType === 8)
-        .find((v) => v.textContent?.includes(comment))!;
-      document.head.insertBefore(style, commentNode);
-    };
-    injectStyleTo('preflight-injection-point', v.default);
-  });
-} else {
-  import('./styles/tailwind-base.sass');
-}
-
-
 const root = createRoot(document.getElementById('root')!);
 root.render(<App />);
